b473bcbd8c6dc8d4a84e4631b286bccf261cf8d1
Adrian Haynes <adhay88@berkeley.edu>
2022-11-05 01:34:35 +0000

    Part 2

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,6 +4,6 @@ class Movie < ActiveRecord::Base
   # implement this method. Remeber to exclude [self]
   # (the current movie) from your return value
   def others_by_same_director
-    return Movie.where(director: self.director).where.not(title: self.title)
+    return Movie.where(:director => director) - [self]
   end
 end
